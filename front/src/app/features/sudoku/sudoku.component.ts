84e02f655f0de720d70f9dc6a82a95e272120e34
IshtamarFr <ishta@ishtamar.fr>
2024-05-03 13:37:01 +0200

    html for sudoku works

--- front/src/app/features/sudoku/sudoku.component.ts
+++ front/src/app/features/sudoku/sudoku.component.ts
@@ -1,13 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sudoku',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './sudoku.component.html',
   styleUrl: './sudoku.component.scss',
 })
 export class SudokuComponent {
-  S: (number | null)[][] = Array(9).fill(Array(9).fill(null));
+  S: (number | null)[] = Array(81).fill(0);
+
+  test(): void {
+    window.alert(this.S.toString());
+  }
+
+  verify(rank: number): void {
+    if (![null, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(this.S[rank]))
+      this.S[rank] = null;
+  }
 }
