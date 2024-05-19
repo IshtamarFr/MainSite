43cdc0bcf264f6b6a3e48f3d10ff26f6b476e0a5
IshtamarFr <ishta@ishtamar.fr>
2024-05-19 16:20:20 +0200

    router seems to work for gestmdp

--- front/src/app/features/gestmdp/gestmdp.routes.ts
+++ front/src/app/features/gestmdp/gestmdp.routes.ts
@@ -3,4 +3,5 @@ import { GestmdpComponent } from './components/main/gestmdp.component';
 
 export const GESTMDP_ROUTES: Routes = [
   { path: '', component: GestmdpComponent },
+  { path: ':categoryId', component: GestmdpComponent },
 ];
