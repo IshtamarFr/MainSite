725e28ebeaee270206545766bdf895b0e523a75c
IshtamarFr <ishta@ishtamar.fr>
2024-05-25 18:27:29 +0200

    /gestmdp -> /gestmdp/0 for router

--- front/src/app/features/gestmdp/gestmdp.routes.ts
+++ front/src/app/features/gestmdp/gestmdp.routes.ts
@@ -3,7 +3,7 @@ import { GestmdpComponent } from './components/main/gestmdp.component';
 import { PasswordFormComponent } from './components/password-form/password-form.component';
 
 export const GESTMDP_ROUTES: Routes = [
-  { path: '', component: GestmdpComponent },
+  { path: '', redirectTo: '0', pathMatch: 'full' },
   { path: 'password', component: PasswordFormComponent },
   { path: 'password/:passwordId', component: PasswordFormComponent },
   { path: ':categoryId', component: GestmdpComponent },
