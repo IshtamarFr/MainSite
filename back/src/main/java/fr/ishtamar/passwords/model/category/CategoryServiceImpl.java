c62dbc8f354a9ca426df1a3b8a7ba4adf8fa05ee
IshtamarFr <ishta@ishtamar.fr>
2024-12-10 18:06:55 +0100

    Standard package works perfectly well

--- back/src/main/java/fr/ishtamar/passwords/model/category/CategoryServiceImpl.java
+++ back/src/main/java/fr/ishtamar/passwords/model/category/CategoryServiceImpl.java
@@ -1,43 +1,18 @@
 package fr.ishtamar.passwords.model.category;
 
-import fr.ishtamar.starter.exceptionhandler.EntityNotFoundException;
 import fr.ishtamar.starter.exceptionhandler.GenericException;
-import fr.ishtamar.starter.model.user.UserInfo;
+import fr.ishtamar.starter.standard.StdEntityRepository;
+import fr.ishtamar.starter.standard.StdEntityService;
+import fr.ishtamar.starter.standard.StdEntityServiceImpl;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class CategoryServiceImpl implements CategoryService {
-    private final CategoryRepository repository;
+public class CategoryServiceImpl extends StdEntityServiceImpl<Category> implements CategoryService {
 
-    public CategoryServiceImpl(CategoryRepository repository){this.repository=repository;}
-
-    @Override
-    public Category createEntity(Category category) throws GenericException {
-        List<Category> candidate=repository.findByNameAndUser(category.getName(),category.getUser());
-
-        if (candidate.isEmpty()) {
-            return repository.save(category);
-        } else {
-            throw new GenericException("This category already exists for this user");
-        }
-    }
-
-    @Override
-    public List<Category> getEntitiesForUser(UserInfo user) {
-        return repository.findByUser(user);
-    }
-
-    @Override
-    public Category getEntityById(Long id) throws EntityNotFoundException {
-        return repository.findById(id)
-                .orElseThrow(()-> new EntityNotFoundException(Category.class,"id",id.toString()));
-    }
-
-    @Override
-    public void deleteEntity(Category category) {
-        repository.delete(category);
+    public CategoryServiceImpl(StdEntityRepository<Category> repository) {
+        super(repository);
     }
 
     @Override
